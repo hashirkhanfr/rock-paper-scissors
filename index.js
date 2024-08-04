490f2139f6bdf68a3c54b1036f024669900694b0
hashirkhanfr <hashirkhanfr@gmail.com>
2024-08-04 21:48:24 +0500

    Code the getRandomNumber() function

---
+++ index.js
@@ -0,0 +1,4 @@
+function getRandomNumer(){
+    return (Math.floor(Math.random() * 3))  //Writing 3 as the max number will return either 0,1 or 2.
+    //Math.floor will round down the float result to the nearest lower integer
+} 
