00a117a3d8fdc90e31e8c25152b6cefdb5a0b7fc
hashirkhanfr <hashirkhanfr@gmail.com>
2024-08-04 21:53:09 +0500

    Add getComputerChoice() to return random number 0,1 or 2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function getRandomNumer(){
-    return (Math.floor(Math.random() * 3))  //Writing 3 as the max number will return either 0,1 or 2.
+    return (Math.floor(Math.random() * 3));  //Writing 3 as the max number will return either 0,1 or 2.
     //Math.floor will round down the float result to the nearest lower integer
 } 
+
+function getComputerChoice(){
+    let computerSelection = getRandomNumer();
+}
