96217d89f2e2c724144f5c009c13533499e8fbc0
hashirkhanfr <hashirkhanfr@gmail.com>
2024-08-10 13:03:05 +0500

    Remove comments and comment out all logs + alerts

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function getRandomNumer(){
-    return (Math.floor(Math.random() * 3));  // Writing 3 as the max number will return either 0,1 or 2.
-    // Math.floor will round down the float result to the nearest lower integer
+    return (Math.floor(Math.random() * 3));  
 } 
 
 function getComputerChoice(){
@@ -8,52 +7,52 @@ function getComputerChoice(){
 }
 
 function getUserChoice(count){
-    let stringChoice = prompt(`--------Round ${count}--------\nRock,Paper or Scissors?`);  // holds the user choice in a string form.
-    let finalChoice;                                       // to hold the integer value for user's choice to later compare with computer's choice
-    switch(stringChoice.toLowerCase()){                    // converting string to lowercase to handle exceptions, and then switching it.
-        case "rock":        finalChoice = 0; break;        // 0 = Rock
-        case "paper":       finalChoice = 1; break;        // 1 = Paper
-        case "scissors":    finalChoice = 2; break;        // 2 = Scissors
+    // let stringChoice = prompt(`--------Round ${count}--------\nRock,Paper or Scissors?`); 
+    let finalChoice;                                       
+    switch(stringChoice.toLowerCase()){                    
+        case "rock":        finalChoice = 0; break;        
+        case "paper":       finalChoice = 1; break;       
+        case "scissors":    finalChoice = 2; break;      
     }
     return (finalChoice);
 }
 
 function playRound(myChoice,botChoice){
-    let printArray = ["Rock","Paper","Scissors"];          //To avoid writing the exact terms in console.log again and again
+    let printArray = ["Rock","Paper","Scissors"];         
     if ((myChoice === 0 && botChoice === 2) || (myChoice === 1 && botChoice === 0) || (myChoice === 2 && botChoice === 1))
     {
-        console.log("You Win! " + printArray[myChoice] + " beats " + printArray[botChoice]);
-        return ('W');   // For my winning conditions, it's a W
+        // console.log("You Win! " + printArray[myChoice] + " beats " + printArray[botChoice]);
+        return ('W');  
     }
         
     else if ((myChoice === 0 && botChoice === 1) || (myChoice === 1 && botChoice === 2) || (myChoice === 2 && botChoice === 0))
     {
-        console.log("You Lose! " + printArray[botChoice] + " beats " + printArray[myChoice]);
-        return ('L');   // For my losing conditions, it's an L
+        // console.log("You Lose! " + printArray[botChoice] + " beats " + printArray[myChoice]);
+        return ('L');   
     }
     else
     {
-        console.log("It's a Tie! " + printArray[botChoice] + " ties with " + printArray[myChoice]);
-        return ('T');   // It will be a tie if no one wins or loses
+        // console.log("It's a Tie! " + printArray[botChoice] + " ties with " + printArray[myChoice]);
+        return ('T'); 
     }
         
 }
 
 function playGame(maxGames){
     for (let i = 1; i <= maxGames; i++){
-        console.log("------ROUND " + i + "------");
-        const humanSelection = getUserChoice(i);        //Calls function which prompts user for input and returns the respective integer 0,1 or 2
-        const computerSelection = getComputerChoice(); //Calls function which generates random number between 0 - 2
-        switch(playRound(humanSelection,computerSelection)){       // playing the main round and returning the result to increment scores
+        // console.log("------ROUND " + i + "------");
+        const humanSelection = getUserChoice(i);       
+        const computerSelection = getComputerChoice(); 
+        switch(playRound(humanSelection,computerSelection)){      
             case 'W' : humanScore++    ; break;
             case 'L' : computerScore++ ; break;
         }     
     }
-    alert(`Player's Score: ${humanScore}` + `\nComputer's Score: ${computerScore}`);   // alerts with the final result of the game
+    // alert(`Player's Score: ${humanScore}` + `\nComputer's Score: ${computerScore}`);  
 
 }
 
-let humanScore = 0                             // For storing user's score after each round
-   ,computerScore = 0;                         // For storing computer's score after each round
+let humanScore = 0                            
+   ,computerScore = 0;                        
 
-playGame(5);                                   //plays the game for 5 rounds
+playGame(5);                             
