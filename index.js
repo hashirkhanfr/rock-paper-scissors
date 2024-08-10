88306b7b815d52686846f0c725232eb55c6fdd62
hashirkhanfr <hashirkhanfr@gmail.com>
2024-08-10 21:24:35 +0500

    Integrate results display between scorecard in DOM

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);  
     document.querySelector(".rock-box.b-choice").style.backgroundColor = "unset";
     document.querySelector(".paper-box.b-choice").style.backgroundColor = "unset";
     document.querySelector(".scissors-box.b-choice").style.backgroundColor = "unset";
-    let choice = Math.floor(Math.random() * 3);  
     switch (choice){
         case 0:
-            document.querySelector(".rock-box.b-choice").style.backgroundColor = "rgb(0, 150, 219)";
+            document.querySelector(".rock-box.b-choice").style.backgroundColor = "red";
             break;
         case 1:
-            document.querySelector(".paper-box.b-choice").style.backgroundColor = "rgb(0, 150, 219)";
+            document.querySelector(".paper-box.b-choice").style.backgroundColor = "red";
             break;
         case 2:
-            document.querySelector(".scissors-box.b-choice").style.backgroundColor = "rgb(0, 150, 219)";
+            document.querySelector(".scissors-box.b-choice").style.backgroundColor = "red";
             break;
     }
     return choice;
 } 
 
-function playRound(myChoice,botChoice){
-    let printArray = ["Rock","Paper","Scissors"];         
-    if ((myChoice === 0 && botChoice === 2) || (myChoice === 1 && botChoice === 0) || (myChoice === 2 && botChoice === 1))
-        displayResult('W');  
-        
-    else if ((myChoice === 0 && botChoice === 1) || (myChoice === 1 && botChoice === 2) || (myChoice === 2 && botChoice === 0))
-        displayResult('L');   
-    else
-        displayResult('T');   
+function playRound(myChoice,botChoice){    
+        if ((myChoice === 0 && botChoice === 2) || (myChoice === 1 && botChoice === 0) || (myChoice === 2 && botChoice === 1))
+        {
+            roundResult.innerHTML = `<span style="color: rgb(0, 173, 216);">Player</span> wins the round`;
+            displayResult('W');
+        }
+        else if ((myChoice === 0 && botChoice === 1) || (myChoice === 1 && botChoice === 2) || (myChoice === 2 && botChoice === 0))
+        {
+            roundResult.innerHTML = `<span style="color: red;">Computer</span> wins the round`;
+            displayResult('L');   
+        }
+
+        else
+        {
+            roundResult.innerHTML= "It is a Tie!!!";
+            displayResult('T'); 
+        }
+             
 }
 
 function displayResult(charac){
@@ -35,13 +44,14 @@ function displayResult(charac){
     else if (charac === 'L'){
         botLabel.textContent = ++computerScore;
     }
+
 }
 
 let playerLabel = document.querySelector(".player-results");
 let botLabel = document.querySelector(".bot-results");
-
+let roundResult = document.querySelector(".round-results");
 let humanScore = 0                            
-   ,computerScore = 0;                        
+   ,computerScore = 0;  
 
 document.querySelector(".rock-box").addEventListener("click",function(){
     playRound(0,getComputerChoice())
@@ -52,5 +62,3 @@ document.querySelector(".paper-box").addEventListener("click",function(){
 document.querySelector(".scissors-box").addEventListener("click",function(){
     playRound(2,getComputerChoice())
 })                             
-
-
